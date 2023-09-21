2a2440a799af5a9aa067ba39c74a1e98e2395d92
Theresa Li <tli4@atlassian.com>
2023-09-21 21:04:28 +1000

    adding cloudfront

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -107,3 +107,57 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.ecs.arn
   }
 }
+
+locals {
+  alb_origin_id = "my-alb"
+}
+
+
+resource "aws_cloudfront_distribution" "cf_distribution" {
+  origin {
+    domain_name = aws_lb.my_alb.dns_name
+    origin_id   = aws_lb.my_alb.dns_name
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "My CloudFront distribution"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_lb.my_alb.dns_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    #acm_certificate_arn      = aws_acm_certificate.alb_cert.arn
+    #ssl_support_method       = "sni-only"
+    #minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+}
